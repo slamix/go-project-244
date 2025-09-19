02c155ed721651e0f73282855520855e87f4de8f
slamix <lokomotivsapsan@gmail.com>
2025-09-19 11:38:12 +0300

    init project and did first step

---
+++ Makefile
@@ -0,0 +1,5 @@
+build:
+	go build -o bin/gendiff ./cmd/gendiff
+
+lint:
+	golangci-lint run
---
+++ cmd/gendiff/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	cli "github.com/urfave/cli/v3"
+)
+
+func main() {
+	cmd := &cli.Command{
+		Name:  "gendiff",
+		Usage: "Compares two configuration files and shows a difference.",
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			return nil
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
